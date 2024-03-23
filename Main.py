5115f0f712fbe9ba60f3d70203fe05c8193296ec
Samuel <Samuelbattistoni541@gmail.com>
2024-03-23 18:19:03 +0100

    Commit

--- Main.py
+++ Main.py
@@ -1,1 +1,16 @@
-print("ciao mondo"),
+def questionario():
+    print("Benvenuto al questionario!")
+    risposta = input("Preferisci pizza o pasta? ").lower()
+
+    if risposta == "pizza":
+        print("Ottima scelta!")
+    elif risposta == "pasta":
+        print("Anche la pasta Ã¨ una scelta deliziosa!")
+    else:
+        print("Scelta non valida. Per favore rispondi con 'pizza' o 'pasta'.")
+        questionario()
+
+
+questionario()
+ 
+ 
